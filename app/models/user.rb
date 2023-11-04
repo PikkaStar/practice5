81e82d21c5e4e457c97c208c0835d1fbd57d16cb
PikkaStar <yokotaka1226@gmail.com>
2023-11-04 07:55:36 +0000

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
          has_many :favorites,dependent: :destroy
          has_many :book_comments,dependent: :destroy
 
+         has_many :relationships,foreign_key: "follow_id",dependent: :destroy
+         has_many :reverse_of_relationships,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
+         has_many :followings,through: :relationships,source: :follower
+         has_many :followers,through: :reverse_of_relationships,source: :follow
 
          def get_profile_image(width,height)
            if profile_image.attached?
@@ -18,4 +22,32 @@ class User < ApplicationRecord
              "no_image"
            end
          end
-end
+
+         def follow(user_id)
+           relationships.create(follower_id: user_id)
+         end
+
+         def unfollow(user_id)
+           relationships.find_by(follower_id: user_id).destroy
+         end
+
+         def following?(user)
+           followings.include?(user)
+         end
+
+         def self.looks(search,word)
+           if search == "perfect_match"
+              @user = User.where("name LIKE?","#{word}")
+              elsif search == "forward_match"
+              @user = User.where("name LIKE?","#{word}%")
+              elsif search == "backward_match"
+              @user = User.where("name LIKE?","%#{word}")
+              elsif search == "partial_match"
+              @user = User.where("name LIKE?","%#{word}%")
+              else
+              @user = User.all
+              end
+          end
+         end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get root to: 'homes#top'
   get "home/about"=>'homes#about',as: 'about'
   resources :users,only: [:index,:show,:edit,:update] do
+    resource :relationships,only: [:create,:destroy]
+    get "followings"=>"relationships#followings",as: "followings"
+    get "followers"=>"relationships#followers",as: "followers"
     member do
       get :favorite
     end
@@ -12,5 +15,6 @@ Rails.application.routes.draw do
     resource :favorite,only: [:create,:destroy]
     resources :book_comments,only: [:create,:destroy]
   end
+  get "searches"=>"searches#search",as: "search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
