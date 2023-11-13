92ab684a307c3968eef11a8b18c16b3e380a5873
PikkaStar <yokotaka1226@gmail.com>
2023-11-13 04:21:01 +0000

    muri

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.user_id = current_user.id
     @message.save
-    redirect_to request.referer
   end
 
    private
